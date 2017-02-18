51660c1408b0c5170cb81fd337abcd689381241d
Ther <emeraldvision@users.noreply.github.com>
2017-02-17 23:38:15 -0600

    created list of times

--- app/src/main/java/com/emeraldvision/scheduler/CalendarActivity.java
+++ app/src/main/java/com/emeraldvision/scheduler/CalendarActivity.java
@@ -7,6 +7,10 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CalendarActivity extends AppCompatActivity {
 
     private Calendar myCalendar;
@@ -16,6 +20,13 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_calendar);
+
+        // list of time IDs
+        List<Integer> timeList = Arrays.asList(R.id.time_0, R.id.time_1, R.id.time_2, R.id.time_3,
+                R.id.time_4, R.id.time_5, R.id.time_6, R.id.time_7, R.id.time_8, R.id.time_9,
+                R.id.time_10, R.id.time_11, R.id.time_12, R.id.time_13, R.id.time_14, R.id.time_15,
+                R.id.time_16, R.id.time_17, R.id.time_18, R.id.time_19, R.id.time_20, R.id.time_21,
+                R.id.time_22, R.id.time_23, R.id.time_24);
 
         // add labels to each hour's view
         setTextOnView(R.id.time_0, R.string.time_0);
@@ -51,8 +62,8 @@ public class CalendarActivity extends AppCompatActivity {
         RelativeLayout.LayoutParams taskParams = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
-        taskParams.addRule(RelativeLayout.ALIGN_TOP, R.id.time_1);
-        taskParams.addRule(RelativeLayout.ABOVE, R.id.time_23);
+        taskParams.addRule(RelativeLayout.ALIGN_TOP, R.id.time_0);
+        taskParams.addRule(RelativeLayout.ABOVE, R.id.time_24);
         taskParams.setMargins(getResources().getDimensionPixelSize(R.dimen.hour_width),
                 getResources().getDimensionPixelSize(R.dimen.line_height), 0, 0);
         task.setLayoutParams(taskParams);
