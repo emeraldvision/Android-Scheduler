8f7d4ae040860a016aae3982b585b4b7c0933863
Ther <emeraldvision@users.noreply.github.com>
2017-02-22 20:59:23 -0600

    New Event Activity has functional date picker

---
+++ app/src/main/java/com/emeraldvision/scheduler/DatePickerFragment.java
@@ -0,0 +1,45 @@
+package com.emeraldvision.scheduler;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import org.joda.time.DateTime;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    long longDate;
+    private DatePickerDialog.OnDateSetListener listener;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        longDate = getArguments().getLong("longTime");
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        DateTime d = new DateTime(longDate);
+        int year = d.getYear();
+        int month = d.getMonthOfYear() - 1;  // jodatime uses 1-12 while Android uses 0-11
+        int day = d.getDayOfMonth();
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        // Listen for date choice
+        listener.onDateSet(view, year, month + 1, day);   // jodatime uses 1-12 while Android uses 0-11
+    }
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+}
--- app/src/main/java/com/emeraldvision/scheduler/NewEventActivity.java
+++ app/src/main/java/com/emeraldvision/scheduler/NewEventActivity.java
@@ -1,16 +1,22 @@
 package com.emeraldvision.scheduler;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 
+
 public class NewEventActivity extends AppCompatActivity {
+
+    DateTime start;
+    DateTime end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +28,16 @@ public class NewEventActivity extends AppCompatActivity {
         }
 
         // initialize date as current date
-        DateTime start = new DateTime();
-        int dateID = R.id.new_event_date;
-        String dateString = start.toString(DateTimeFormat.longDate());
-        setTextOnView(dateID, dateString);
-        TextView dateTextView = (TextView) findViewById(dateID);
+        start = new DateTime();
+        final long longTime = start.getMillis();
+        int dateID = displayDate();
+        View dateView = findViewById(dateID);
+        dateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(longTime);
+            }
+        });
 
         // initialize start as current time
         int startID = R.id.start_time;
@@ -34,14 +45,37 @@ public class NewEventActivity extends AppCompatActivity {
         setTextOnView(startID, startString);
 
         // initialize end 1 hour after start
-        DateTime end = start.plusHours(1);
+        end = start.plusHours(1);
         int endID = R.id.end_time;
         String endString = end.toString(DateTimeFormat.shortTime());
         setTextOnView(endID, endString);
     }
 
+    private int displayDate() {
+        int dateID = R.id.new_event_date;
+        String dateString = start.toString(DateTimeFormat.longDate());
+        setTextOnView(dateID, dateString);
+        return dateID;
+    }
+
     // add time to display
     private void setTextOnView(int viewID, String stringID) {
         ((TextView) findViewById(viewID)).setText(stringID);
+    }
+
+    // show Date Picker
+    private void showDatePickerDialog(long longTime) {
+        DatePickerFragment dateFragment = new DatePickerFragment();
+        Bundle arguments = new Bundle();
+        arguments.putLong("longTime", longTime);
+        dateFragment.setArguments(arguments);
+        dateFragment.show(getSupportFragmentManager(), getString(R.string.datePicker));
+        dateFragment.setListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                start = start.withDate(year, month, dayOfMonth);
+                displayDate();
+            }
+        });
     }
 }
